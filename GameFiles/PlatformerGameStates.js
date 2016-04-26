d1ac0fc08dc72cb77f9094fe561185271d627e5c
Redpix_ <dzavashviligeorge@gmail.com>
2016-04-26 22:46:59 +0400

    Yep Hearts and Stuff too :>>

--- GameFiles/PlatformerGameStates.js
+++ GameFiles/PlatformerGameStates.js
@@ -18,14 +18,19 @@ var PlatformerGameStates;
             this.game.load.image("crystal", AssetsFolder + "Crystal.png");
             this.game.load.image("ground", AssetsFolder + "Block_Ground.png");
             this.game.load.spritesheet("player", AssetsFolder + "dude.png", 32, 48);
+            this.game.load.image("trap", AssetsFolder + "trap.png");
+            this.game.load.image("heart", AssetsFolder + "heart.png");
             var MusicsFolder = "GameFiles/Music/";
             this.game.load.audio("crystalpickup", MusicsFolder + "crystal.ogg");
             this.game.load.audio("bgmusic", MusicsFolder + "bgmusic.ogg");
+            this.game.load.audio("trap", MusicsFolder + "trap.ogg");
         };
         GameRunningState.prototype.create = function () {
             // Create cursor keys
             this.cursors = this.game.input.keyboard.createCursorKeys();
             this.game.world.setBounds(0, 0, 4000, this.game.world.height);
+            // Hearts
+            this.hearts = 3;
             // Activate arcade physics engine :>
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
             this.game.physics.arcade.gravity.y = 1000;
@@ -51,7 +56,7 @@ var PlatformerGameStates;
             this.addLedge(3200, this.game.world.height - 64 - (this.game.world.height * 0.38), "ground", 0.8, 1);
             this.addLedge(3600, this.game.world.height - 64 - (this.game.world.height * 0.5), "ground", 0.8, 1);
             this.addLedge(3900, this.game.world.height - 64 - (this.game.world.height * 0.6), "ground", 0.8, 1);
-            var instr = this.game.add.text(window.innerWidth + 20, 20, "Collect all the Fallen Crystals...", { font: "Garamond", fontSize: "80px", fill: "#999999" });
+            var instr = this.game.add.text(window.innerWidth + 20, 40, "Collect all the Fallen Crystals...", { font: "Garamond", fontSize: "80px", fill: "#999999" });
             // Player
             this.player = this.game.add.sprite(32, this.game.world.height - 150, "player");
             // Enable physics for the player
@@ -66,15 +71,39 @@ var PlatformerGameStates;
             // Add Stars
             this.crystals = this.game.add.group();
             this.crystals.enableBody = true;
+            this.traps = this.game.add.group();
+            this.traps.enableBody = true;
+            this.heartsGroup = this.game.add.group();
+            this.heartsGroup.enableBody = true;
+            for (var i = 0; i < this.hearts; i++) {
+                var h = this.heartsGroup.create(window.innerWidth - 40 - (40 * i), 20, "heart");
+                h.fixedToCamera = true;
+                h.body.immovable = true;
+                h.body.allowGravity = false;
+            }
+            this.numberOfCrystals = 0;
             for (var i = 0; i < 58; i++) {
-                var star = this.crystals.create(i * 70, 0, "crystal");
-                // Set some gravity
-                star.body.gravity.y = 6;
-                star.scale.set(1.1);
-                // Collide with world bounds
-                star.body.collideWorldBounds = true;
-                // Different bounce values
-                star.body.bounce.y = 0.7 * Math.random() * 0.2;
+                var crystalOrTrap = Math.floor(Math.random() * 2);
+                if (crystalOrTrap) {
+                    var crystal = this.crystals.create(i * 70, 0, "crystal");
+                    // Set some gravity
+                    crystal.body.gravity.y = 6;
+                    crystal.scale.set(1.1);
+                    // Collide with world bounds
+                    crystal.body.collideWorldBounds = true;
+                    // Different bounce values
+                    crystal.body.bounce.y = 0.7 * Math.random() * 0.2;
+                    this.numberOfCrystals++;
+                }
+                else {
+                    var trap = this.traps.create(i * 70, 0, "trap");
+                    // Set gravity
+                    trap.body.gravity.y = 100;
+                    trap.scale.set(1);
+                    trap.anchor.set(0.5);
+                    trap.body.colliweWorldBounds = true;
+                    trap.body.bounce.y = 0.6 * Math.random() * 0.2;
+                }
             }
             // Set score to zero
             this.score = 0;
@@ -84,6 +113,7 @@ var PlatformerGameStates;
             this.game.camera.follow(this.player);
             // Add Music
             this.pickupsound = this.game.add.audio("crystalpickup");
+            this.trapsound = this.game.add.audio("trap");
             this.bgmusic = this.game.add.audio("bgmusic");
             this.bgmusic.onDecoded.add(this.startMusic, this);
         };
@@ -99,7 +129,9 @@ var PlatformerGameStates;
         GameRunningState.prototype.update = function () {
             this.game.physics.arcade.collide(this.player, this.platforms);
             this.game.physics.arcade.collide(this.crystals, this.platforms);
+            this.game.physics.arcade.collide(this.traps, this.platforms);
             this.game.physics.arcade.overlap(this.player, this.crystals, this.collectStar, null, this);
+            this.game.physics.arcade.overlap(this.player, this.traps, this.trapped, null, this);
             // Reset Players velocity movement
             this.player.body.velocity.x = 0;
             // Left Right movement
@@ -121,10 +153,23 @@ var PlatformerGameStates;
             if (this.cursors.up.isDown && this.player.body.touching.down) {
                 this.player.body.velocity.y = -700;
             }
-            if (this.score >= 58 * 10) {
+            if (this.score >= this.numberOfCrystals * 10) {
                 this.game.world.bounds.setTo(0, 0, window.innerWidth, window.innerHeight);
                 this.game.state.start("GameWonState", true, true);
             }
+        };
+        GameRunningState.prototype.trapped = function (player, trap) {
+            trap.kill();
+            this.trapsound.play(null, 0, 0.7);
+            if (this.hearts > 0) {
+                this.hearts--;
+            }
+            if (this.hearts <= 0) {
+                this.hearts = 0;
+                this.game.state.start("GameLostState", true, true, this.score);
+            }
+            this.game.world.bounds.setTo(0, 0, window.innerWidth, window.innerHeight);
+            this.heartsGroup.removeChildAt(this.hearts);
         };
         GameRunningState.prototype.collectStar = function (player, star) {
             star.kill();
@@ -191,7 +236,7 @@ var PlatformerGameStates;
     PlatformerGameStates.GameStartState = GameStartState;
     var GameLostState = (function (_super) {
         __extends(GameLostState, _super);
-        function GameLostState(score) {
+        function GameLostState() {
             _super.call(this);
         }
         GameLostState.prototype.init = function (score) {
@@ -207,13 +252,12 @@ var PlatformerGameStates;
                 fill: "#333355"
             });
             this.endText.anchor.set(0.5);
-            this.endText.smoothed = false;
+            //this.endText.smoothed = false;
             this.restart = this.game.add.button(this.game.world.centerX, this.game.world.centerY, "restartButton", this.startGame, this);
-            this.endText.anchor.set(0.5);
-            this.endText.scale.set(1.4);
+            this.restart.anchor.set(0.5);
+            this.restart.scale.set(1.4);
         };
         GameLostState.prototype.startGame = function () {
-            this.game.time.events.events = [];
             this.game.state.start("GameRunningState", true, true);
         };
         return GameLostState;
--- GameFiles/PlatformerGameStates.ts
+++ GameFiles/PlatformerGameStates.ts
@@ -30,10 +30,20 @@ module PlatformerGameStates {
         // PickUpSound
         pickupsound: Phaser.Sound;
         bgmusic: Phaser.Sound;
+        trapsound: Phaser.Sound;
 
         // Instructions text
         instr: Phaser.Text;
 
+        // Traps
+        traps: Phaser.Group;
+
+        // Hearts
+        hearts: number;
+        heartsGroup: Phaser.Group;
+
+        // Number of crystals
+        numberOfCrystals: number;
 
         constructor() {
             super();
@@ -46,16 +56,22 @@ module PlatformerGameStates {
             this.game.load.image("crystal", AssetsFolder + "Crystal.png");
             this.game.load.image("ground", AssetsFolder + "Block_Ground.png");
             this.game.load.spritesheet("player", AssetsFolder + "dude.png", 32, 48);
+            this.game.load.image("trap", AssetsFolder + "trap.png");
+            this.game.load.image("heart", AssetsFolder + "heart.png");
 
             var MusicsFolder = "GameFiles/Music/";
             this.game.load.audio("crystalpickup", MusicsFolder + "crystal.ogg");
             this.game.load.audio("bgmusic", MusicsFolder + "bgmusic.ogg");
+            this.game.load.audio("trap", MusicsFolder + "trap.ogg");
         }
 
         create() {
             // Create cursor keys
             this.cursors = this.game.input.keyboard.createCursorKeys();
             this.game.world.setBounds(0, 0, 4000, this.game.world.height);
+
+            // Hearts
+            this.hearts = 3;
 
             // Activate arcade physics engine :>
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -101,7 +117,7 @@ module PlatformerGameStates {
 
             this.addLedge(3900, this.game.world.height - 64 - (this.game.world.height * 0.6), "ground", 0.8, 1);
 
-            var instr = this.game.add.text(window.innerWidth + 20, 20, "Collect all the Fallen Crystals...", { font: "Garamond", fontSize: "80px", fill: "#999999" });
+            var instr = this.game.add.text(window.innerWidth + 20, 40, "Collect all the Fallen Crystals...", { font: "Garamond", fontSize: "80px", fill: "#999999" });
 
             // Player
             this.player = this.game.add.sprite(32, this.game.world.height - 150, "player");
@@ -122,18 +138,52 @@ module PlatformerGameStates {
             this.crystals = this.game.add.group();
             this.crystals.enableBody = true;
 
+            this.traps = this.game.add.group();
+            this.traps.enableBody = true;
+
+            this.heartsGroup = this.game.add.group();
+            this.heartsGroup.enableBody = true;
+
+            for (var i = 0; i < this.hearts; i++) {
+                var h: Phaser.Sprite = this.heartsGroup.create(window.innerWidth - 40 - (40 * i), 20, "heart");
+                h.fixedToCamera = true;
+                h.body.immovable = true;
+                h.body.allowGravity = false;
+            }
+
+            this.numberOfCrystals = 0;
+
             for (var i = 0; i < 58; i++){
-                var star = this.crystals.create(i * 70, 0, "crystal");
 
-                // Set some gravity
-                star.body.gravity.y = 6;
-                star.scale.set(1.1);
+                var crystalOrTrap: number = Math.floor(Math.random() * 2);
 
-                // Collide with world bounds
-                star.body.collideWorldBounds = true;
+                if (crystalOrTrap) {
+                    var crystal = this.crystals.create(i * 70, 0, "crystal");
 
-                // Different bounce values
-                star.body.bounce.y = 0.7 * Math.random() * 0.2;
+                    // Set some gravity
+                    crystal.body.gravity.y = 6;
+                    crystal.scale.set(1.1);
+
+                    // Collide with world bounds
+                    crystal.body.collideWorldBounds = true;
+
+                    // Different bounce values
+                    crystal.body.bounce.y = 0.7 * Math.random() * 0.2;
+
+                    this.numberOfCrystals++;
+                } else {
+                    var trap = this.traps.create(i * 70, 0, "trap");
+
+                    // Set gravity
+                    trap.body.gravity.y = 100;
+                    trap.scale.set(1);
+                    trap.anchor.set(0.5);
+
+                    trap.body.colliweWorldBounds = true;
+
+                    trap.body.bounce.y = 0.6 * Math.random() * 0.2; 
+                }
+                
             }
 
             // Set score to zero
@@ -148,6 +198,7 @@ module PlatformerGameStates {
 
             // Add Music
             this.pickupsound = this.game.add.audio("crystalpickup");
+            this.trapsound = this.game.add.audio("trap");
             this.bgmusic = this.game.add.audio("bgmusic");
 
             this.bgmusic.onDecoded.add(this.startMusic, this);
@@ -165,10 +216,14 @@ module PlatformerGameStates {
             this.bgmusic.play(undefined, 0, 0.9, true);
         }
 
+
+
         update() {
             this.game.physics.arcade.collide(this.player, this.platforms);
             this.game.physics.arcade.collide(this.crystals, this.platforms);
+            this.game.physics.arcade.collide(this.traps, this.platforms);
             this.game.physics.arcade.overlap(this.player, this.crystals, this.collectStar, null, this);
+            this.game.physics.arcade.overlap(this.player, this.traps, this.trapped, null, this);
 
             // Reset Players velocity movement
             this.player.body.velocity.x = 0;
@@ -192,10 +247,26 @@ module PlatformerGameStates {
                 this.player.body.velocity.y = -700;
             }
 
-            if (this.score >= 58 * 10) {
+            if (this.score >= this.numberOfCrystals * 10) {
                 this.game.world.bounds.setTo(0, 0, window.innerWidth, window.innerHeight);
                 this.game.state.start("GameWonState", true, true);
             }
+        }
+
+        trapped(player: Phaser.Sprite, trap: Phaser.Sprite) {
+            trap.kill();
+            this.trapsound.play(null, 0, 0.7); 
+            if (this.hearts > 0){
+                this.hearts--;
+            }
+            if (this.hearts <= 0){
+                this.hearts = 0;
+                this.game.state.start("GameLostState", true, true, this.score);
+            }
+
+            this.game.world.bounds.setTo(0, 0, window.innerWidth, window.innerHeight);
+            this.heartsGroup.removeChildAt(this.hearts);
+            
         }
 
         collectStar(player: Phaser.Sprite, star: Phaser.Sprite) {
@@ -284,7 +355,7 @@ module PlatformerGameStates {
         restart: Phaser.Button;
         score: number;
 
-        constructor(score: number) {
+        constructor() {
             super();
         }
 
@@ -303,19 +374,19 @@ module PlatformerGameStates {
                 fill: "#333355"
             });
             this.endText.anchor.set(0.5);
-            this.endText.smoothed = false;
+            //this.endText.smoothed = false;
+
             this.restart = this.game.add.button(
                 this.game.world.centerX,
                 this.game.world.centerY,
                 "restartButton",
                 this.startGame,
                 this);
-            this.endText.anchor.set(0.5);
-            this.endText.scale.set(1.4);
+            this.restart.anchor.set(0.5);
+            this.restart.scale.set(1.4);
         }
 
         startGame() {
-            this.game.time.events.events = [];
             this.game.state.start("GameRunningState", true, true);
         }
     }
